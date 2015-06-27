c1773b2493300ab7be024f8c0b82dfac0bfd258a
joker <gaidukov.denis@gmail.com>
2015-06-27 23:33:57 +0300

    Refactored fabfile + removed makefile

--- fabfile.py
+++ fabfile.py
@@ -18,10 +18,10 @@ env.password = config.get('production-server', 'password')
 env.prompts = {}
 # ===========================================================
 
-# project settings ==========================================
-project_user = config.get('project', 'user')
-project_user_group = config.get('project', 'group')
-project_folder = config.get('project', 'folder')
+# application settings ======================================
+project_user = config.get('application', 'user')
+project_user_group = config.get('application', 'group')
+project_folder = config.get('application', 'folder')
 # ===========================================================
 
 # database settings =========================================
@@ -55,16 +55,16 @@ env.prompts.update(django_superuser_prompts)
 # ===========================================================
 
 system_packages = (
-    'python-dev',
-    'make',
     'gcc',
+    'make',
+    'python-dev',
     'libpq-dev',
     'python-virtualenv',
-    'git',
     'postgresql',
     'postgresql-contrib',
     'nginx',
-    'supervisor'
+    'supervisor',
+    'git',
 )
 
 
@@ -73,21 +73,27 @@ def install_system_packages():
     run('apt-get install {}'.format(system_packages_list))
 
 
-def create_project_owner():
+def create_system_user_for_application():
     run('groupadd --system {}'.format(project_user_group))
     run(
         'useradd --system --gid {} --shell /bin/bash --home {} {}'.format(
             project_user_group, project_folder, project_user
         )
     )
-
-
-def create_project_folder_structure():
     run('mkdir -p {}'.format(project_folder))
     run('chown {} {}'.format(project_user, project_folder))
 
 
-def create_project_database():
+def create_application_folders_structure():
+    with cd(project_folder):
+        run('mkdir -p logs')
+        run('mkdir -p run')
+        run('mkdir -p media_content')
+        run('touch logs/gunicorn_supervisor.log;')
+        run('chmod u+x bin/gunicorn_start.sh;')
+
+
+def create_application_database():
     sudo(
         'createuser --interactive -P',
         user='postgres'
@@ -98,63 +104,92 @@ def create_project_database():
     )
 
 
-def get_project_from_github():
+def clone_application_from_repository():
     with cd(project_folder):
-        run('pwd')
         run('git clone {} .'.format(github_repository))
 
 
-def change_project_files_owner():
+def change_application_files_owner():
     run('chown -R {}:{} {}'.format(project_user, project_user_group, project_folder))
     run('chmod -R g+w {}'.format(project_folder))
 
 
-def build_project_environment():
+def create_application_environment():
     with cd(project_folder):
-        run('chmod u+x Makefile')
-        run('make create-core-environment')
+        run('rm -rf env')
+        run('virtualenv env')
 
 
-def django_management_commands():
+def install_application_packages():
+    with cd(project_folder):
+        run('env/bin/pip install --requirement requirements.txt')
+
+
+def process_application_migrations():
     with cd(project_folder):
         run('env/bin/python2.7 src/manage.py makemigrations')
         run('env/bin/python2.7 src/manage.py migrate')
-        run('env/bin/python2.7 src/manage.py collectstatic --noinput')
 
 
-def django_create_superuser():
+def process_application_static_files():
+    with cd(project_folder):
+        run('env/bin/python2.7 src/manage.py collectstatic')
+
+
+def create_application_superuser():
     with cd(project_folder):
         run('env/bin/python2.7 src/manage.py createsuperuser --username {}'.format(django_superuser_username))
 
 
 def install_supervisor_config():
     with cd(project_folder):
-        run('cp config/supervisor/internships.conf /etc/supervisor/conf.d/internships.conf')
+        run('cp --update config/supervisor/internships.conf /etc/supervisor/conf.d/internships.conf')
         run('supervisorctl reread')
         run('supervisorctl update')
         run('supervisorctl restart all')
 
 
+def restart_supervisor():
+    run('supervisorctl restart all')
+
+
 def install_nginx_config():
     with cd(project_folder):
-        run('cp config/nginx/internships.conf /etc/nginx/sites-available/internships.conf')
-        run('ln -s /etc/nginx/sites-available/internships.conf /etc/nginx/sites-enabled/internships.conf')
-        run('service nginx restart')
+        run('cp --update config/nginx/internships.conf /etc/nginx/sites-available/internships.conf')
+        run('ln -s -f /etc/nginx/sites-available/internships.conf /etc/nginx/sites-enabled/internships.conf')
 
-def get_last_project_version():
+
+def restart_nginx():
+    run('service nginx restart')
+
+
+def get_last_application_version():
     with cd(project_folder):
         run('git pull origin master')
-        change_project_files_owner()
+        change_application_files_owner()
 
 
-def clean_deploy():
+def update():
+    get_last_application_version()
+    change_application_files_owner()
+    restart_supervisor()
+    restart_nginx()
+
+
+def clear_install():
     install_system_packages()
-    create_project_database()
-    create_project_owner()
-    create_project_folder_structure()
-    get_project_from_github()
-    build_project_environment()
-    django_management_commands()
-    django_create_superuser()
-    change_project_files_owner()
+    create_application_database()
+    create_system_user_for_application()
+    create_application_folders_structure()
+    clone_application_from_repository()
+    create_application_environment()
+    install_application_packages()
+    process_application_migrations()
+    process_application_static_files()
+    create_application_superuser()
+    change_application_files_owner()
     install_supervisor_config()
+    restart_supervisor()
+    change_application_files_owner()
+    install_nginx_config()
+    restart_nginx()
