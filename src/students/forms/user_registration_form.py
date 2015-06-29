42b081b570d8522e250676f191f9754d9fd43c3a
Denis Gaidukov <gaidukov.denis@gmail.com>
2015-06-29 18:35:38 +0300

    Added new theme + created basic registration process

---
+++ src/students/forms/user_registration_form.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from django.core import validators
+from django.contrib.auth.models import User
+
+
+class UserRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        exclude = ('is_active', 'username', 'date_joined',)
+
+    email = forms.EmailField(
+        required=True,
+        validators=[validators.EmailValidator]
+    )
+    first_name = forms.CharField(
+        required=True,
+        validators=[validators.RegexValidator(r'^[a-zA-Z]{1,50}$')]
+    )
+    last_name = forms.CharField(
+        required=True,
+        validators=[validators.RegexValidator(r'^[a-zA-Z]{1,50}$')]
+    )
+    password = forms.CharField(required=True)
+    password_confirmation = forms.CharField(required=True)
+
+    def clean_password_confirmation(self):
+        password = self.cleaned_data.get('password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if not password_confirmation:
+            raise forms.ValidationError("You must confirm your password")
+
+        if password != password_confirmation:
+            raise forms.ValidationError("Your passwords do not match")
--- src/students/views/registration_view.py
+++ src/students/views/registration_view.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import View
 from django.shortcuts import render
-
-from students.forms.registration_form import RegistrationForm
+from students.forms.user_registration_form import UserRegistrationForm
 
 
 class RegistrationView(View):
     template_name = 'students/registration.html'
-    form = RegistrationForm
+    form = UserRegistrationForm
 
     def get(self, request):
         context = {'form': self.form()}
@@ -16,6 +15,10 @@ class RegistrationView(View):
     def post(self, request):
         form = self.form(request.POST)
         if form.is_valid():
-            print 'valid'
+            user = form.save(commit=False)
+            user.username = user.email
+            user.set_password(user.password)
+            user.save()
+
         context = {'form': form}
         return render(request, self.template_name, context)
