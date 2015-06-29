38eac7748fcf4e1ccd470142b6645949c425a7bd
Denis Gaidukov <gaidukov.denis@gmail.com>
2015-06-29 17:18:14 +0300

    Migraions + updated provinces fixtures: deleted community

--- src/common/models/province.py
+++ src/common/models/province.py
@@ -4,10 +4,9 @@ from django.db import models
 
 class Province(models.Model):
     name = models.CharField(max_length=50)
-    autonomous_community = models.CharField(max_length=50)
 
     def __unicode__(self):
-        return ', '.join((self.name, self.autonomous_community))
+        return self.name
 
     def __str__(self):
         return unicode(self)
