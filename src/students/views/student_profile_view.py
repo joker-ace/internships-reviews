68490e3432ca0022f67f60ab6f31be5845ea6c22
Denis Gaidukov <gaidukov.denis@gmail.com>
2015-07-10 19:41:44 +0300

    Added handler for student university page

---
+++ src/students/views/student_profile_view.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+from common.views.common_base_view import CommonBaseView
+from common.models.university import University
+from common.models.faculty import Faculty
+from students.models.student import Student
+
+
+class StudentProfileView(CommonBaseView):
+    template_name = 'students/profile.html'
+
+    @method_decorator(login_required)
+    def get(self, request):
+        self.update_context({
+            'universities': University.objects.all()
+        })
+        return self.response()
+
+    @method_decorator(login_required)
+    def post(self, request):
+        university = University.objects.get(pk=request.POST.get('university'))
+        faculty = request.POST.get('faculty')
+
+        try:
+            Faculty.objects.get(pk=faculty, university=university)
+        except (Faculty.DoesNotExist, ValueError):
+            faculty = Faculty.create(faculty, university)
+            faculty.save()
+
+        student = Student()
+        student.university = university
+        student.faculty = faculty
+        student.is_studying = bool(request.POST.get('is_styding'))
+        student.user = request.user
+        student.save()
+        return self.redirect_to('companies_list_page')
